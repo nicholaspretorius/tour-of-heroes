3d16a0586873353104f82dc26bd1bf4840b14ab8
Nicholas Pretorius <nicholaspretorius@gmail.com>
2018-12-30 15:07:28 +0200

    Added routing and dashboard.

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './../app.hero';
+import { HeroService } from './../hero.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  heroes: Hero[] = [];
+  numTop = 4;
+
+  constructor(private readonly heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes.slice(0, this.numTop));
+  }
+
+}
